2e02512ce12ed59f2d61fb2b9b17b3d335e30b1c
lucaz248 <lucas.walkowicz@gmail.com>
2023-01-30 13:17:39 +0100

    Add Brand entity. Refactor Price.

--- src/main/java/com/store/repository/PriceRepository.java
+++ src/main/java/com/store/repository/PriceRepository.java
@@ -4,6 +4,7 @@ import com.store.entity.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 /**
  * The Price repository.
  */
+@Repository
 public interface PriceRepository extends JpaRepository<Price, Long> {
 
     /**
