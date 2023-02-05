579dfb2292564900d0b242b8de09cf94a282aa6b
lucaz248 <lucas.walkowicz@gmail.com>
2023-02-05 17:59:12 +0100

    Add PriceService test

--- src/test/java/com/store/serviceImpl/PriceServiceImplTest.java
+++ src/test/java/com/store/serviceImpl/PriceServiceImplTest.java
@@ -7,22 +7,16 @@ import com.store.service.PriceService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
-@SpringBootTest(classes = {PriceServiceImpl.class})
+@SpringBootTest()
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@ContextConfiguration(classes = {PriceServiceImpl.class, ModelMapper.class})
 class PriceServiceImplTest {
 
     @Autowired
