5d64d428f6bf209a5993c0d64d194f35c3e0cb34
lucaz248 <lucaz248@hotmail.com>
2023-02-05 18:42:54 +0100

    Add Const class

---
+++ src/main/java/com/store/consts/Consts.java
@@ -0,0 +1,13 @@
+package com.store.consts;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * The applications constants.
+ */
+public class Consts {
+    /**
+     * The dateTimeFormater used to recover price
+     */
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm");
+}
